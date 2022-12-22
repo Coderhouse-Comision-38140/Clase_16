e9e3862adf9910d4b2a177a457aa625e37027024
jpromanonet <programming@jpromano.net>
2022-12-22 11:41:30 -0300

    deleteCarsWhere

--- Ejemplo/deleteCarsWhere.js
+++ Ejemplo/deleteCarsWhere.js
@@ -3,7 +3,7 @@ const { options } = require("./options/mysql.js")
 const knex = require("knex")(options)
 
 /* Funcionalidades */
-knex.from("cars").del()
+knex.from("cars").where("price", ">", "500").del()
     .then(() => console.log("Todos los autos han sido eliminados"))
     .catch((err) => {console.log(err); throw err})
     .finally(() => {
