8e99091e38ab255ea599fbe2148b9ea2d07c8253
jpromanonet <programming@jpromano.net>
2022-12-22 11:40:05 -0300

    deleteCars

---
+++ Ejemplo/deleteCars.js
@@ -0,0 +1,12 @@
+/* Importamos nuestras dependencias */
+const { options } = require("./options/mysql.js")
+const knex = require("knex")(options)
+
+/* Funcionalidades */
+knex.from("cars").del()
+    .then(() => console.log("Todos los autos han sido eliminados"))
+    .catch((err) => {console.log(err); throw err})
+    .finally(() => {
+        knex.destroy()
+    })
+
